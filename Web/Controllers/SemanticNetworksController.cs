73a89e03aef16cf7f71e05755927acf60c80d93e
permyak <nextzucker@gmail.com>
2014-12-07 15:38:31 +0600

    Добавил заглушку получения всех семсетей.

--- Web/Controllers/SemanticNetworksController.cs
+++ Web/Controllers/SemanticNetworksController.cs
@@ -1,18 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-namespace Web.Controllers
+﻿namespace Web.Controllers
 {
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Web.Http;
+    using SemanticNetworkKernel;
+
     public class SemanticNetworksController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        private IEnumerable<SemanticNetwork> getNewSemanticNetworks()
         {
-            return new string[] { "value1", "value2" };
+            var list = new List<SemanticNetwork>
+            {
+                new SemanticNetwork { Name = "network1" },
+                new SemanticNetwork { Name = "network2" }
+            };
+
+            var vertexA = new Vertex { Text = "a" };
+            var vertexB = new Vertex { Text = "b" };
+            var vertexC = new Vertex { Text = "c" };
+            var vertexD = new Vertex { Text = "d" };
+            var vertexE = new Vertex { Text = "e" };
+            var vertexF = new Vertex { Text = "f" };
+
+            list[0].Vertices = new Collection<Vertex> { vertexA, vertexB, vertexC, vertexD, vertexE, vertexF };
+
+            list[0].Arcs = new Collection<Arc>();
+            list[0].Arcs.Add(new Arc { Text = "edge#1", FromVertex = vertexA, ToVertex = vertexB });
+            list[0].Arcs.Add(new Arc { Text = "edge#2", FromVertex = vertexA, ToVertex = vertexC });
+            list[0].Arcs.Add(new Arc { Text = "edge#3", FromVertex = vertexA, ToVertex = vertexD });
+            list[0].Arcs.Add(new Arc { Text = "edge#4", FromVertex = vertexA, ToVertex = vertexE });
+            list[0].Arcs.Add(new Arc { Text = "edge#5", FromVertex = vertexA, ToVertex = vertexF });
+
+            return list;
+        }
+
+        // GET api/<controller>
+        public IEnumerable<SemanticNetwork> Get()
+        {
+            return getNewSemanticNetworks();
         }
 
         // GET api/<controller>/5
