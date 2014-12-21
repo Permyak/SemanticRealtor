ec0c4a1b03101e90451af5f4c6695bb172c17a30
apechenezhsky <nextzucker@gmail.com>
2014-12-22 02:56:58 +0600

    Добавил api-операции для семсети.

--- Web/Controllers/SemanticNetworksController.cs
+++ Web/Controllers/SemanticNetworksController.cs
@@ -2,71 +2,40 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
     using System.Web.Http;
     using SemanticNetworkKernel;
 
     public class SemanticNetworksController : ApiController
     {
-        private SemanticNetworkContext _context;
-
-        private IEnumerable<SemanticNetwork> getNewSemanticNetworks()
-        {
-            var list = new List<SemanticNetwork>
-            {
-                new SemanticNetwork { Name = "network1" },
-                new SemanticNetwork { Name = "network2" }
-            };
-
-            return list;
-        }
-
-        private SemanticNetwork getNewSemanticNetwork()
-        {
-            var semNet = new SemanticNetwork();
-
-            var vertexA = new Vertex { Text = "a", SemanticNetwork = semNet};
-            var vertexB = new Vertex { Text = "b" };
-            var vertexC = new Vertex { Text = "c" };
-            var vertexD = new Vertex { Text = "d" };
-            var vertexE = new Vertex { Text = "e" };
-            var vertexF = new Vertex { Text = "f" };
-
-            semNet.Vertices = new Collection<Vertex> { vertexA, vertexB, vertexC, vertexD, vertexE, vertexF };
-
-            semNet.Arcs = new Collection<Arc>();
-            semNet.Arcs.Add(new Arc { Text = "edge#1", FromVertex = vertexA, ToVertex = vertexB });
-            semNet.Arcs.Add(new Arc { Text = "edge#2", FromVertex = vertexA, ToVertex = vertexC });
-            semNet.Arcs.Add(new Arc { Text = "edge#3", FromVertex = vertexA, ToVertex = vertexD });
-            semNet.Arcs.Add(new Arc { Text = "edge#4", FromVertex = vertexA, ToVertex = vertexE });
-            semNet.Arcs.Add(new Arc { Text = "edge#5", FromVertex = vertexA, ToVertex = vertexF });
-
-            return semNet;
-        }
-
+        private SemanticNetworkContext _context = new SemanticNetworkContext();
+        
         // GET api/<controller>
         public IEnumerable<SemanticNetwork> Get()
         {
-            return new SemanticNetworkContext().SemanticNetworks;
+            return _context.SemanticNetworks;
         }
 
         // GET api/<controller>/5
         public SemanticNetwork Get(int id)
         {
-            return new SemanticNetworkContext().SemanticNetworks.First(x => x.SemanticNetworkId == id);
+            var semNetwork = _context.SemanticNetworks.Find(id);
+            if (semNetwork == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return semNetwork;
         }
 
         // POST api/<controller>
-        public void Post([FromBody]string name)
+        public void Post([FromBody]SemanticNetwork semNetwork)
         {
-            if (!string.IsNullOrEmpty(name))
+            if (semNetwork != null)
             {
-                var semNetwork = new SemanticNetwork { Name = name };
-                _context = new SemanticNetworkContext();
                 _context.SemanticNetworks.Add(semNetwork);
                 _context.SaveChanges();
-                _context.Dispose();
             }
             else
             {
@@ -75,13 +44,32 @@
         }
 
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]SemanticNetwork newSemNetwork)
         {
+            var semNetwork = _context.SemanticNetworks.Find(id);
+            if (semNetwork == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            semNetwork.Name = newSemNetwork.Name;
+            semNetwork.Vertices = newSemNetwork.Vertices;
+            semNetwork.Arcs = newSemNetwork.Arcs;
+
+            _context.SaveChanges();
         }
 
         // DELETE api/<controller>/5
         public void Delete(int id)
         {
+            var semNetwork = _context.SemanticNetworks.Find(id);
+            if (semNetwork == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            _context.SemanticNetworks.Remove(semNetwork);
+            _context.SaveChanges();
         }
     }
 }
